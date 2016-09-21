a2e5f7e2ff3ff91bb5a328534de6acd27640b1a9
Felix Dorner <felix.dorner@gmail.com>
2016-09-21 12:37:51 +0200

    [1179] Fix NPE during XML Pivot Export
    A two year old change in EMFs EcoreUtil.Copier breaks the model
    transformation: Required EAnnotations on target EPackages are missing
    and that causes an NPE during export. 
    
    See also https://bugs.eclipse.org/bugs/show_bug.cgi?id=432082
    
    Bug: 1179
    Change-Id: Ia0c95f3a0dc8a3ea21ab00f058f0ad3ca19082b5
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- plugins/org.polarsys.capella.xmlpivot.extraction/src/org/polarsys/capella/xmlpivot/extraction/extraction/MMCleaner.java
+++ plugins/org.polarsys.capella.xmlpivot.extraction/src/org/polarsys/capella/xmlpivot/extraction/extraction/MMCleaner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -70,22 +70,6 @@ public class MMCleaner implements IMMCleaner {
       }
     }
 
-    // Suppress all annotations except the xmlpivot annotation on EPackages
-    // !! DOESN'T IMPACT THE INITIAL MAPPING
-    for (EModelElement element : _invertibleRelationMapping.getValues(EModelElement.class)){
-      for (Iterator<EAnnotation> it = element.getEAnnotations().iterator(); it.hasNext();){
-        EAnnotation next = it.next();
-        if (!XMLPivotMetadata.SOURCE_XMLPIVOT.equals(next.getSource())){
-          it.remove();
-        }
-      }
-      if (element instanceof EEnum){
-        for (EEnumLiteral l : (((EEnum) element).getELiterals())){
-          l.getEAnnotations().clear();
-        }
-      }
-    }
-    
   }
 
   /**
--- plugins/org.polarsys.capella.xmlpivot.extraction/src/org/polarsys/capella/xmlpivot/extraction/mapping/MMInvertibleRelationCopyMappingGenerator.java
+++ plugins/org.polarsys.capella.xmlpivot.extraction/src/org/polarsys/capella/xmlpivot/extraction/mapping/MMInvertibleRelationCopyMappingGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -90,16 +90,22 @@ public class MMInvertibleRelationCopyMappingGenerator implements IMMCopyMappingG
         super.copyAttribute(eAttribute, eObject, copyEObject);
       }
     }
-    
 
     @Override
-    protected EObject createCopy(EObject eObject){
-      EObject copy = super.createCopy(eObject);
-      if (eObject instanceof EPackage){
-        // for EPackages we want to remember where the simplified package came from.
-        XMLPivotMetadata.INSTANCE.setSourceNsURI((EPackage) copy, ((EPackage) eObject).getNsURI());
+    protected void copyContainment(EReference eReference, EObject eObject, EObject copyEObject) {
+
+      // Do not copy EAnnotations
+      if (eReference == EcorePackage.Literals.EMODEL_ELEMENT__EANNOTATIONS){ 
+
+        // for EPackages remember where the simplified package came from.
+        if (eObject instanceof EPackage) {        
+          XMLPivotMetadata.INSTANCE.setSourceNsURI((EPackage) copyEObject, ((EPackage) eObject).getNsURI());
+        }
+
+      } else {
+        super.copyContainment(eReference, eObject, copyEObject);
       }
-      return copy;
+
     }
   }
 
